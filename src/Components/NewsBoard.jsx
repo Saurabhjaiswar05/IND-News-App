c0e9ff9442b71a7924e39b3d8ee1611cc476b38d
Saurabhjaiswar05 <sjsaurabh082@gmail.com>
2024-06-14 10:05:35 +0530

    added try and catch

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,26 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import NewsItem from './NewsItem';
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
 
-const NewsBoard = ({category}) => {
-    const [articles, setArticles] = useState([]);
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
 
-    useEffect(()=>{
-      try {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=459d573dccaa4d5b99034fed92c004ff`
-        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
-      } catch (error) {
-        console.log(error);
+  useEffect(() => {
+    
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=459d573dccaa4d5b99034fed92c004ff`;
+    fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
-        
-    },[category])
+      return response.json();
+    })
+      .then((data) => setArticles(data.articles))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [category]);
   return (
-    <div style={{paddingLeft:"110px", backgroundColor:"black",paddingTop:"45px"}}>
-      <h2 className='text-center pt-5 ' style={{color:"white"}}>Latest <span className='badge bg-danger'>News</span></h2>
-      {articles.map((news,index)=>{
-        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+    <div
+      style={{
+        paddingLeft: "110px",
+        backgroundColor: "black",
+        paddingTop: "45px",
+      }}
+    >
+      <h2 className="text-center pt-5 " style={{ color: "white" }}>
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default NewsBoard
+export default NewsBoard;
